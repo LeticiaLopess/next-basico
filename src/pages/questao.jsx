17764e191d5f2c964cc37b5f3236b96de76b8a65
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2022-12-15 21:45:52 -0300

    integração com API

---
+++ src/pages/questao.jsx
@@ -0,0 +1,33 @@
+import { useEffect } from "react"
+import { useState } from "react"
+
+export default function questao() {
+    const [questao, setQuestao] = useState()
+    
+    useEffect(() => {
+        fetch('http://localhost:3000/api/questao/123')
+    .then(resp => resp.json())
+    .then(setQuestao)
+    }, [])
+
+    function renderizarRespostas() {
+        if(questao) {
+            return questao.respostas.map((resp, i) => {
+                return <li key={i}>{resp}</li>
+            })
+        }
+        return false
+    }
+
+    return (
+        <div>
+            <h1>Questão</h1>
+            <div>
+                <span>{questao?.id} - {questao?.enunciado}</span>
+            </div>
+            <ul>
+                {renderizarRespostas()}
+            </ul>
+        </div>
+    )
+}
